0b69cfebc73f25a56ef6f870d39a669cd524603e
KaikOliveira <69028381+KaikOliveira@users.noreply.github.com>
2021-01-28 20:28:07 -0300

    getAPi

--- frontend/src/pages/ListClient/index.jsx
+++ frontend/src/pages/ListClient/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../service/api';
 
 import Header from '../../components/Header/index';
 
@@ -7,9 +8,21 @@ import {
   ContentContainer,
   Status,
   SearchContainer,
+  ListClients,
+  List,
+  Name,
+  AddressAndContact,
 } from './styles';
 
 export default function PageList() {
+  const [dataClient, setDataClient] = useState([]);
+
+  useEffect(() => {
+    api.get('listar').then((response) => {
+      setDataClient(response.data);
+    });
+  }, [dataClient]);
+
   return (
     <Wrapper>
       <Header />
@@ -18,6 +31,61 @@ export default function PageList() {
         <Status />
 
         <SearchContainer />
+
+        <ListClients>
+          {dataClient.map((client) => (
+            <List key={client.id}>
+              <Name>{client.nome}</Name>
+              <AddressAndContact>
+                <p>
+                  <strong>Tipo de pessoa:</strong>
+                  {' '}
+                  {client.tipopessoa}
+                </p>
+                <p>
+                  <strong>Cpf/Cnpj: </strong>
+                  {' '}
+                  {client.cpf_cnpj}
+                </p>
+                <p>
+                  <strong>Nome: </strong>
+                  {' '}
+                  {client.nome}
+                </p>
+                <p>
+                  <strong>Endereço: </strong>
+                  {' '}
+                  {client.endereco}
+                </p>
+                <p>
+                  <strong>Numero: </strong>
+                  {' '}
+                  {client.nro_logradouro}
+                </p>
+                <p>
+                  <strong>Bairro: </strong>
+                  {' '}
+                  {client.bairro}
+                </p>
+                <p>
+                  <strong>Cidade: </strong>
+                  {' '}
+                  {client.cidade}
+                </p>
+                <p>
+                  <strong>UF: </strong>
+                  {' '}
+                  {client.uf}
+                </p>
+                <p>
+                  <strong>Cep: </strong>
+                  {' '}
+                  {client.cep}
+                </p>
+              </AddressAndContact>
+            </List>
+          ))}
+        </ListClients>
       </ContentContainer>
     </Wrapper>
   );
--- frontend/src/pages/ListClient/styles.js
+++ frontend/src/pages/ListClient/styles.js
@@ -30,3 +30,42 @@ export const SearchContainer = styled.div`
   margin-left: 2.5%;
   margin-top: 15px;
 `;
+
+export const ListClients = styled.div`
+  height: 37%;
+  width: 95%;
+  background: #123456;
+  margin-left: 2.5%;
+  margin-top: 15px;
+`;
+
+export const List = styled.li`
+  margin-top: 5px;
+  margin-bottom: 5px;
+  list-style: none;
+  width: 90%;
+  background: #fff;
+  border-radius: 20px;
+  border: solid 0.3px #03A9F4;
+  transition: background-color 0.3s;
+  &:hover {
+    background: #E1F5FE;
+  }
+`;
+
+export const Name = styled.h3`
+  text-align: center;
+  font-weight: bold;
+`;
+
+export const AddressAndContact = styled.div`
+  margin-left: 15px;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  p {
+    font-size: 14px;
+    bold{
+      font-weight: 600;
+    }
+  }
+`;
