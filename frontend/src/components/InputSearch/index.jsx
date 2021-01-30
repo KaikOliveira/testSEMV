59eccfca1b6d0d3a2176dc2f8861f9bfbc7af074
KaikOliveira <69028381+KaikOliveira@users.noreply.github.com>
2021-01-30 03:09:51 -0300

    startSearch

---
+++ frontend/src/components/InputSearch/index.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useCallback } from 'react';
+
+import { Container } from './styles';
+
+export default function Input({
+  onChange,
+  placeholder,
+  value,
+
+}) {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(true);
+  }, []);
+
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+
+    </Container>
+  );
+}
